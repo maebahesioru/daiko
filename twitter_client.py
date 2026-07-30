269f4f4c9d8e452e34cd0bd7a124b6187323d64a
maebahesioru <pikomaeba@gmail.com>
2026-07-30 18:48:06 +0900

    fix: don't delete media until post succeeds (retry-safe)

--- twitter_client.py
+++ twitter_client.py
@@ -92,12 +92,6 @@ class TwitterClient:
             log.info("Uploading image: %s", filename)
             media_id = await client.upload_media(path)
         log.info("Media uploaded: %s -> %s", filename, media_id)
-        # Delete local file after successful upload
-        try:
-            os.remove(path)
-            log.info("Deleted local file: %s", path)
-        except OSError:
-            pass
         return media_id
 
     async def post_tweet(self, text: str, media_filenames: str = "[]",
